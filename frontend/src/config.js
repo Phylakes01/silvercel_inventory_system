e0b0f7ef0d03423442424d5ad2f2bf4d5cd35eda
Edcel <lero.edceljohnlorenz.m@gmail.com>
2025-11-12 11:51:00 +0800

    updated config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 
 const isProduction = process.env.NODE_ENV === "production";
+const PRODUCTION_URL = import.meta.env.PRODUCTION_URL;
 
 export const API_BASE_URL = isProduction
-	? "https://your-production-domain.com/backend/api" // Replace with your production URL
-	: "http://localhost/react_inventory_system/backend/api";
+	? PRODUCTION_URL // Replace with your production URL
+	: "http://localhost/silvercel_inventory_system/backend/api";
